74d2228c9f0b48af00a1b4b3f549894fad38e8e2
Orhngn <120358745+Orhngn@users.noreply.github.com>
2023-03-08 20:51:15 +0300

    js-language

---
+++ seasons.js
@@ -0,0 +1,67 @@
+function seasonOpen() {
+  document.getElementById("season").style.display = "block";
+  document.getElementById("spring").style.display = "none";
+  document.getElementById("summer").style.display = "none";
+  document.getElementById("autumn").style.display = "none";
+  document.getElementById("winter").style.display = "none";
+}
+function springOpen() {
+  document.getElementById("season").style.display = "none";
+  document.getElementById("spring").style.display = "block";
+  document.getElementById("summer").style.display = "none";
+  document.getElementById("autumn").style.display = "none";
+  document.getElementById("winter").style.display = "none";
+}
+function summerOpen() {
+  document.getElementById("season").style.display = "none";
+  document.getElementById("spring").style.display = "none";
+  document.getElementById("summer").style.display = "block";
+  document.getElementById("autumn").style.display = "none";
+  document.getElementById("winter").style.display = "none";
+}
+function autumnOpen() {
+  document.getElementById("season").style.display = "none";
+  document.getElementById("spring").style.display = "none";
+  document.getElementById("summer").style.display = "none";
+  document.getElementById("autumn").style.display = "block";
+  document.getElementById("winter").style.display = "none";
+}
+function winterOpen() {
+  document.getElementById("season").style.display = "none";
+  document.getElementById("spring").style.display = "none";
+  document.getElementById("summer").style.display = "none";
+  document.getElementById("autumn").style.display = "none";
+  document.getElementById("winter").style.display = "block";
+}
+function cevirİng() {
+  document.getElementById("ing").style.backgroundColor = "blue";
+  document.getElementById("ing").style.color = "white";
+  document.getElementById("tr").style.backgroundColor = "transparent";
+  document.getElementById("tr").style.color = "black";
+  document.getElementById("lang-ing").style.display = "block";
+  document.getElementById("lang-ing1").style.display = "block";
+  document.getElementById("lang-ing2").style.display = "block";
+  document.getElementById("lang-ing3").style.display = "block";
+  document.getElementById("lang-ing4").style.display = "block";
+  document.getElementById("lang-tr").style.display = "none";
+  document.getElementById("lang-tr1").style.display = "none";
+  document.getElementById("lang-tr2").style.display = "none";
+  document.getElementById("lang-tr3").style.display = "none";
+  document.getElementById("lang-tr4").style.display = "none";
+}
+function cevirTr() {
+  document.getElementById("ing").style.backgroundColor = "transparent";
+  document.getElementById("ing").style.color = "black";
+  document.getElementById("tr").style.backgroundColor = "blue";
+  document.getElementById("tr").style.color = "white";
+  document.getElementById("lang-ing").style.display = "none";
+  document.getElementById("lang-ing1").style.display = "none";
+  document.getElementById("lang-ing2").style.display = "none";
+  document.getElementById("lang-ing3").style.display = "none";
+  document.getElementById("lang-ing4").style.display = "none";
+  document.getElementById("lang-tr").style.display = "block";
+  document.getElementById("lang-tr1").style.display = "block";
+  document.getElementById("lang-tr2").style.display = "block";
+  document.getElementById("lang-tr3").style.display = "block";
+  document.getElementById("lang-tr4").style.display = "block";
+}
